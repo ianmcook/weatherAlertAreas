ed310afeaf753e7d6c95da08846c5f0c88530ed7
Ian Cook <ianmcook@gmail.com>
2016-03-11 19:58:30 -0500

    Originate package weatherAlertAreas version 0.1-0

---
+++ R/alertAreas.R
@@ -0,0 +1,14 @@
+#' Alert Areas Used by the National Weather Service
+#'
+#' Polygons defining the states, counties, and zones used by the United States
+#' National Weather Service (\acronym{NWS}) to define alert areas.
+#'
+#' @format A \code{\link[sp:SpatialPolygons-class]{SpatialPolygons}} object of
+#'   length 7350 whose names are 2-letter state abbreviations and 6-character
+#'   Universal Geographic Code (\acronym{UGC}) county and zone codes. Polygons
+#'   are specified in WGS84 coordinates.
+#' @details Package will be periodically updated following updates to alert
+#'   areas by the National Weather Service.
+#' @source \url{http://www.nws.noaa.gov/geodata/}
+#' @seealso Package \pkg{weatherAlerts}
+"alertAreas"
